ae09c14c528d1b7694f10556dc9fdc632a356658
Daniel Castellanos <Castellanos2018*>
2019-03-13 16:35:05 -0500

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemTodoComponent } from './pages/todo/item-todo/item-todo.component';
+import { ListTodoComponent } from './pages/todo/list-todo/list-todo.component';
+import { FooterTodoComponent } from './pages/todo/footer-todo/footer-todo.component';
+import { TodoComponent } from './pages/todo/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemTodoComponent,
+    ListTodoComponent,
+    FooterTodoComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
